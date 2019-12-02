e06e663959b57d832d8dff1d26b3e9da98e436ba
lambda1 <breakthrough.dodorara@gmail.com>
2019-12-03 00:25:33 +0900

    first commit

---
+++ tracer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody m_rigidbody = null;
+
+    /* パラメータ */
+    [SerializeField] private float m_speed = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float move_x = Input.GetAxis("Horizontal");
+        float move_z = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(move_x, 0.0f, move_z);
+
+        m_rigidbody.AddForce(movement * m_speed);
+    }
+}
