0df3698a3d1f033a9aeb3de757d7cefb9691dbfd
lambda1 <breakthrough.dodorara@gmail.com>
2019-12-04 01:24:27 +0900

    fix

---
+++ tracer/Assets/Scripts/KeyItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItem : MonoBehaviour
+{
+    [SerializeField] private Rigidbody m_rigidbody = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.AngleAxis(1.0f , Vector3.up);
+    }
+}
