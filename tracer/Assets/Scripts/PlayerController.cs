717c528d6c8106712cdd2dbbd7c3bb1b1352d5b8
lambda1 <breakthrough.dodorara@gmail.com>
2019-12-03 15:26:43 +0900

    input key

--- tracer/Assets/Scripts/PlayerController.cs
+++ tracer/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // データ定義 
+    // キー入力
+    enum KEY
+    {
+        UP    = KeyCode.W,
+        DOWN  = KeyCode.S,
+        RIGHT = KeyCode.D,
+        LEFT  = KeyCode.A
+    };
+    struct KeyData
+    {
+        public KEY m_key { get; }
+        public Vector3 m_vec { get; }
+        public KeyData(KEY key, Vector3 vec) { m_key = key; m_vec = vec; }
+    }
+
+    // テーブル
+    private List<KeyData> m_key_table = new List<KeyData>();
+
+    // データ
     [SerializeField] private Rigidbody m_rigidbody = null;
 
     /* パラメータ */
     [SerializeField] private float m_speed = 1.0f;
-    
-    // Start is called before the first frame update
+    private Vector3 m_velocity;
+
     void Start()
     {
-        
+        m_velocity = Vector3.zero;
+
+        InitKeyTable();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MoveDirection(); // キー入力による移動
     }
 
     void FixedUpdate()
@@ -28,10 +49,28 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        float move_x = Input.GetAxis("Horizontal");
-        float move_z = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(move_x, 0.0f, move_z);
+    }
 
-        m_rigidbody.AddForce(movement * m_speed);
+    // χ入力による移動処理
+    void MoveDirection()
+    {
+        m_velocity = Vector3.zero;
+        foreach(KeyData data in m_key_table)
+        {
+            if (Input.GetKey((KeyCode)data.m_key))
+            {
+                m_velocity += data.m_vec;
+            }
+        }
+    }
+
+    // 初期化処理
+    // キー入力テーブル
+    void InitKeyTable()
+    {
+        m_key_table.Add(new KeyData(KEY.UP,   new Vector3( 0.0f, 0.0f, 1.0f)));
+        m_key_table.Add(new KeyData(KEY.DOWN, new Vector3( 0.0f, 0.0f,-1.0f)));
+        m_key_table.Add(new KeyData(KEY.RIGHT,new Vector3( 1.0f, 0.0f, 0.0f)));
+        m_key_table.Add(new KeyData(KEY.LEFT, new Vector3(-1.0f, 0.0f, 0.0f)));
     }
 }
